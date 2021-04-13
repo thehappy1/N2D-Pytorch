c019607b97126d66e9db15a17ebd1418affba2f1
Felix <Felix.Schmedes@uni-oldenburg.de>
2021-04-13 11:36:41 +0200

    added fpidataset

--- n2d.py
+++ n2d.py
@@ -342,6 +342,8 @@ if __name__ == "__main__":
 
     label_names = None
     transform = transforms.Compose([transforms.ToTensor(),transforms.Normalize((0.5),(0.5))])
+    if args.dataset == "FashionMNIST":
+        print("works!")
     trainset = torchvision.datasets.FashionMNIST(root='../data', train=True, download=True, transform=transform)
     #trainset = torchvision.datasets.MNIST(root='../data', train=True, download=True, transform=transform)
     trainloader = torch.utils.data.DataLoader(trainset, batch_size=512, shuffle=True, num_workers=16)
